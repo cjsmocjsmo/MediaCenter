2b25a369d11562771767352ea4d18004733437ac
Charlie <porthose.cjsmo@gmail.com>
2020-08-26 11:30:10 -0700

    changed paths

--- service-down.sh
+++ service-down.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
-cd /home/pi/MediaCenter && docker-compose down;
-cd /home/pi/yts-svlt && docker-compose down;
-cd /home/pi/ampvel && docker-compose down;
-cd && docker stop Movies-httpd;
-docker stop TVShows-httpd;
-docker image prune -f;
-docker volume prune -f;
+#!/bin/bash
+cd /home/pi/MediaCenter && docker-compose down && \
+cd /home/pi/yts-svlt && docker-compose down && \
+cd /home/pi/ampvel && docker-compose down && cd && \
+docker stop Movies-httpd && docker stop TVShows-httpd && \
+docker image prune -f && docker volume prune -f
+
--- service-up.sh
+++ service-up.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-docker system prune -f;
-cd /home/pi/MediaCenter && docker-compose up -d;
-cd /home/pi/yts-svlt && docker-compose up -d;
-cd /home/pi/ampvel && docker-compose up -d;
-docker start Movies-httpd;
-docker start TVShows-httpd;
+#!/bin/bash
+docker system prune -f && \
+cd /home/pi/MediaCenter && docker-compose up -d && \
+cd /home/pi/yts-svlt && docker-compose up -d && \
+cd /home/pi/ampvel && docker-compose up -d && \
+docker start Movies-httpd && docker start TVShows-httpd
